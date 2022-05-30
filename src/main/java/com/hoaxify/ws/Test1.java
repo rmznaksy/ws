a1017ffc2fb0a345c521747b39638d059b63550f
Ramazan AKSOY <68793314+rmznaksy@users.noreply.github.com>
2022-05-31 01:03:16 +0300

    ws

---
+++ src/main/java/com/hoaxify/ws/Test1.java
@@ -0,0 +1,10 @@
+package com.hoaxify.ws;
+
+import org.springframework.boot.SpringApplication;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba");
+    }
+}
